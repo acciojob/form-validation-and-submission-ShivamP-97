5930cd5e273ee00a0a04a9762ba96a090998b033
ShivamP-97 <shivampathak19972@gmail.com>
2026-02-13 22:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,23 +9,8 @@ checkbox.addEventListener("change", () => {
 
 
 form.addEventListener("submit", (e) => {
-  const name = form.name.value.trim();
-  const email = form.email.value.trim();
-  const password = form.password.value.trim();
-  const gender = form.gender.value;
-
-  if (!name || !email || !password || !gender) {
-    e.preventDefault();
-    alert("Please complete all fields before submitting.");
-    return;
-  }
-
-
   if (!checkbox.checked) {
     e.preventDefault();
     alert("You must accept the terms and conditions.");
-    return;
   }
-
- 
 });
