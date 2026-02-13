9f72e29845638c939370eef9031f7df2b6f957d8
ShivamP-97 <shivampathak19972@gmail.com>
2026-02-13 22:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// your JS code here
 const form = document.getElementById("userForm");
 const submitBtn = document.getElementById("submitBtn");
 const checkbox = document.querySelector('input[name="terms"]');
+
+submitBtn.disabled = true;
 
 checkbox.addEventListener("change", () => {
   submitBtn.disabled = !checkbox.checked;
 });
 
-
-form.addEventListener("submit", (e) => {
-  if (!checkbox.checked) {
-    e.preventDefault();
-    alert("You must accept the terms and conditions.");
-  }
-});
