335c31d5d4c033129316c8de7488a178c1220e57
ShivamP-97 <shivampathak19972@gmail.com>
2026-02-13 22:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const form = document.getElementById('userForm');
+    const terms = document.getElementById('terms');
+    const submitBtn = document.getElementById('submitBtn');
+
+    terms.addEventListener('change', function () {
+        if (terms.checked) {
+
+            if (form.checkValidity()) {
+                submitBtn.disabled = false;
+            } else {
+                submitBtn.disabled = true;
+                alert("Please fill in all required fields before accepting the terms.");
+                terms.checked = false;
+            }
+
+        } else {
+            submitBtn.disabled = true;
+        }
+    });
